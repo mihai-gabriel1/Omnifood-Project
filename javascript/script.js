75b60e2e744c3bef9db487803b8da85380756f55
mihai-gabriel1 <mr.eu21@gmail.com>
2022-04-29 20:36:18 +0300

    Project finished; a fav icon has been added, mobile navigation optimized.

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,24 @@
-//making mobile nav work & adding a new Date function to display the correct year in the footer
+console.log("Hello world!");
+
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
 const yearEl = document.querySelector(".year");
-const newYear = new Date().getFullYear();
-yearEl.textContent = newYear;
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
@@ -9,52 +26,81 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-//////////////////////////////////////////////////////////
 
-// Making smooth scrolling animation
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
 
+    // Scroll back to top
     if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
 
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
+    // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
 
-// //////////////////////////////////
+///////////////////////////////////////////////////////////
+// Sticky navigation
 
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// const sectionHeroEl = document.querySelector(".section-hero");
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
+
+// ///////////////////////////////////////////////////////////
+// // Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
+
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
+
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
 // checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
