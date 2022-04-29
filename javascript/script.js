6d8466a43dc082652eac43c65a47689b93e2448b
mihai-gabriel1 <mr.eu21@gmail.com>
2022-04-29 15:58:20 +0300

    Made the navigation mobile web responsive; also added smooth scrolling both for safari and other web browsers.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+//making mobile nav work & adding a new Date function to display the correct year in the footer
 const yearEl = document.querySelector(".year");
 const newYear = new Date().getFullYear();
 yearEl.textContent = newYear;
@@ -8,10 +9,35 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-
-//making mobile nav work
-
 //////////////////////////////////////////////////////////
+
+// Making smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// //////////////////////////////////
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
